7dbd2fd4c13e81c65aaab3da4aeedd8c2cf1e82e
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-03-24 15:20:50 +0700

    add carousel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+
+/* import css home */
+import "./Home.css";
+
+/* import components */
 import Navbar from "../../components/Navbar/Navbar";
+import Carousel from "../../components/Carousel/Carousel";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <div className="banner-gradient">
+          <Carousel />
+          <div className="bottom-arrow" />
+        </div>
       </div>
     );
   }
